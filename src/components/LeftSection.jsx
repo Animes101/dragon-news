0d7821d4fd98710ff7b8dc4e47605c38ded65f95
animes-barman <animesbarman101@gmail.com>
2024-04-04 16:58:58 +0600

    complated left side

--- src/components/LeftSection.jsx
+++ src/components/LeftSection.jsx
@@ -1,9 +1,26 @@
-
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const LeftSection = () => {
-  return (
-    <div>LeftSection</div>
-  )
-}
+  const [catagory, setCatagroy] = useState([]);
 
-export default LeftSection
+  useEffect(() => {
+    fetch("/categories.json")
+      .then((res) => res.json())
+      .then((data) => setCatagroy(data));
+  }, []);
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">All Caterogy</h1>
+      <ul className="flex flex-col">
+        {catagory.map((item) => (
+          <NavLink className={`text-lg`} to={``} key={item.id}>
+            {item.name}
+          </NavLink>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default LeftSection;
